0e1ba32f2000413f2cedfd8a9f56bb8e0b1366e9
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2022-03-31 08:09:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,15 +4,27 @@ let taskDiv = document.querySelector(".tasks");
 
 
 //=====================================================
-
-
-
-
-
-
-
-
-
+//background animation
+window.onload = function () {
+  Particles.init({
+    selector: ".background"
+  });
+};
+var particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 100,
+        connectParticles: true
+      }
+    }
+  ]
+});
 
 
 //=====================================================
